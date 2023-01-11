6bb5f275fdb35a0fe8352922365150fe023ccf96
MrHomebrew <Grymee_87@hotmail.com>
2023-01-10 20:50:17 -0500

    Update curl_install.sh

--- curl_install.sh
+++ curl_install.sh
@@ -37,6 +37,7 @@ echo "Starting Service"
 sudo systemctl enable --now force-grub2win-on-next-boot.service
 
 echo "Starting rename of Microsoft to Deck"
+sudo mount /dev/nvme0n1p1 /mnt
 sudo sh -c "cd /mnt; sudo mv /esp/efi/Microsoft /esp/efi/Deck"
 
 echo "Done."
