8a325518dae052b4f3fe292a41b1c85150dfd1ff
MrHomebrew <Grymee_87@hotmail.com>
2023-01-11 00:05:31 -0500

    Update Window_EFI_Rename.sh

--- Window_EFI_Rename.sh
+++ Window_EFI_Rename.sh
@@ -11,24 +11,16 @@
 #curl -sSL https://raw.githubusercontent.com/MrHomebrew/Steam-Deck.Force-Grub2win-On-Boot/main/Window_EFI_Rename.sh | bash
 
 #stop running script if anything returns an error (non-zero exit )
-set -e
 
 repo_url="https://raw.githubusercontent.com/MrHomebrew/Steam-Deck.Force-Grub2win-On-Boot/main"
 
 zenity --question --width=400 \
-
   --text="Read $repo_url/README.md before proceeding. \
-
 \nDo you want to rename the bootmgfw.efi file in esp/efi/Microsoft/boot partition directory to bootmgfw-orig.efi?"
-
 if [ "$?" != 0 ]; then
-
   #NOTE: This code will never be reached due to "set -e", the system will already exit for us but just incase keep this
-
   echo "bye then! xxx"
-
   exit 0;
-
 fi
 
 echo "Starting rename of bootmgfw.efi file to bootmgfw-orig.efi"
