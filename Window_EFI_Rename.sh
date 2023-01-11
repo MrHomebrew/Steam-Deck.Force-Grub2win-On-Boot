e4df34c5617c4a785fc787a5c14366c942786d16
MrHomebrew <Grymee_87@hotmail.com>
2023-01-10 23:24:13 -0500

    Update Window_EFI_Rename.sh

--- Window_EFI_Rename.sh
+++ Window_EFI_Rename.sh
@@ -8,47 +8,9 @@
 
 # Use at own Risk!
 
-#curl -sSL https://raw.githubusercontent.com/MrHomebrew/Steam-Deck.Force-Grub2win-On-Boot/main/curl_install.sh | bash
+#curl -sSL https://raw.githubusercontent.com/MrHomebrew/Steam-Deck.Force-Grub2win-On-Boot/main/Window_EFI_Rename.sh | bash
 
 #stop running script if anything returns an error (non-zero exit )
-
-set -e
-
-repo_url="https://raw.githubusercontent.com/MrHomebrew/Steam-Deck.Force-Grub2win-On-Boot/main"
-
-tmp_dir="/tmp/MrHomebrew.Window_EFI_Rename.install"
-
-zenity --question --width=400 \
-
-  --text="Read $repo_url/README.md before proceeding. \
-
-\nDo you want to install the Force-Grub2win-On-Boot service?"
-
-if [ "$?" != 0 ]; then
-
-  #NOTE: This code will never be reached due to "set -e", the system will already exit for us but just incase keep this
-
-  echo "bye then! xxx"
-
-  exit 0;
-
-fi
-
-echo "Making tmp folder $tmp_dir"
-
-mkdir -p "$tmp_dir"
-
-echo "Downloading Required Files"
-
-curl -o "$tmp_dir/force-grub2win-on-next-boot.service" "$repo_url/force-grub2win-on-next-boot.service"
-
-echo "Copying $tmp_dir/force-grub2win-on-next-boot.service to $service_install_dir/force-grub2win-on-next-boot.service"
-
-sudo cp "$tmp_dir/force-grub2win-on-next-boot.service" "$service_install_dir/force-grub2win-on-next-boot.service"
-
-echo "Starting Service"
-
-sudo systemctl enable --now force-grub2win-on-next-boot.service
 
 zenity --question --width=400 \
 
